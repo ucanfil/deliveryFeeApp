242d7fc5b2170ae2e0d582b2d5921828ce5b97c7
Burak Tilek <burak.tilek@derivco.ee>
2021-10-17 17:10:58 +0300

    Add additional styling for delivery fee

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import { IFee } from "../interfaces/interfaces";
 
 export const Result = ({ fee }: IFee) =>
-  fee ? <div>Delivery fee: {fee}</div> : null;
+  fee ? <div className="result">Delivery fee: {fee}</div> : null;
